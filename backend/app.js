029b75968859a8d7adb80edd9aec7c2e35cdfbf5
Vipin Kumar <demonsorca@pop-os.localdomain>
2023-05-23 02:18:15 +0530

    add get function for testing

--- backend/app.js
+++ backend/app.js
@@ -23,18 +23,22 @@ const user = require("./routes/userRoute");
 const order = require("./routes/orderRoute");
 const payment = require("./routes/paymentRoute");
 
-app.use("/api/v1", product);
-app.use("/api/v1", user);
-app.use("/api/v1", order);
-app.use("/api/v1", payment);
-
-app.use(express.static(path.join(__dirname, "../frontend/build")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+app.get("/", (req, res) => {
+  res.send("Hey this is my API running 🥳");
 });
 
+// app.use("/api/v1", product);
+// app.use("/api/v1", user);
+// app.use("/api/v1", order);
+// app.use("/api/v1", payment);
+
+// app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+// });
+
 // Middleware for Errors
-app.use(errorMiddleware);
+// app.use(errorMiddleware);
 
 module.exports = app;
