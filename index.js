5918e6259c10d562b1f12976ec6579325577e68b
Vipin Kumar <demonsorca@pop-os.localdomain>
2023-05-23 02:40:50 +0530

    update index.js

--- index.js
+++ index.js
@@ -4,44 +4,45 @@ const express = require("express");
 
 const app = express();
 
-const cloudinary = require("cloudinary");
-const connectDatabase = require("./config/database");
+// const cloudinary = require("cloudinary");
+// const connectDatabase = require("./backend/config/database");
 
-// Handling Uncaught Exception
-process.on("uncaughtException", (err) => {
-  console.log(`Error: ${err.message}`);
-  console.log(`Shutting down the server due to Uncaught Exception`);
-  process.exit(1);
-});
+// // Handling Uncaught Exception
+// process.on("uncaughtException", (err) => {
+//   console.log(`Error: ${err.message}`);
+//   console.log(`Shutting down the server due to Uncaught Exception`);
+//   process.exit(1);
+// });
 
-// Config
+// // Config
 if (process.env.NODE_ENV !== "PRODUCTION") {
+  console.log("call this to check the environment variable");
   require("dotenv").config({ path: "backend/config/config.env" });
 }
 
-// Connecting to database
-connectDatabase();
+// // Connecting to database
+// connectDatabase();
 
 app.get("/", (req, res) => {
   res.send("Hey this is my API running 🥳");
 });
 
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
+// cloudinary.config({
+//   cloud_name: process.env.CLOUDINARY_NAME,
+//   api_key: process.env.CLOUDINARY_API_KEY,
+//   api_secret: process.env.CLOUDINARY_API_SECRET,
+// });
 
 const server = app.listen(process.env.PORT, () => {
   console.log(`Server is working on http://localhost:${process.env.PORT}`);
 });
 
 // Unhandled Promise Rejection
-process.on("unhandledRejection", (err) => {
-  console.log(`Error: ${err.message}`);
-  console.log(`Shutting down the server due to Unhandled Promise Rejection`);
+// process.on("unhandledRejection", (err) => {
+//   console.log(`Error: ${err.message}`);
+//   console.log(`Shutting down the server due to Unhandled Promise Rejection`);
 
-  server.close(() => {
-    process.exit(1);
-  });
-});
+//   server.close(() => {
+//     process.exit(1);
+//   });
+// });
